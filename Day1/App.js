bb6961f62e5f6f2199df644aa43c546d92bbd0ce
MunawarSaqibi <mohdmunawar.mar@gmail.com>
2026-07-01 17:04:21 +0530

    js added

--- Day1/App.js
+++ Day1/App.js
@@ -1,11 +1,9 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { sum } from 'lodash';
 
-console.log("hello world")
 
-const App = () => {
-return <h1>Hello World</h1>;
-};
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+function Heading(){
+    return <h1>Hello React</h1>
+}
+ReactDOM.createRoot(document.getElementById("root")).render(<Heading/>);
