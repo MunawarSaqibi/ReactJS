142c7dee584c84f18f82852720b484dbf999c49a
MunawarSaqibi <mohdmunawar.mar@gmail.com>
2026-07-06 16:14:17 +0530

    Css added

--- Day3/main.js
+++ Day3/main.js
@@ -4,8 +4,9 @@ import ReactDOM from "react-dom/client"
 function Header(){
     return(
         <div className="header-container">
-        <div className="logo-div">
-            <img className="img-logo" src="https://ik.imagekit.io/marSaQ/attachment_108678654?updatedAt=1783105327346" alt=""/>
+        <div className="title-div">
+            <img className="img-logo" src="https://ik.imagekit.io/marSaQ/attachment_108678654?updatedAt=1783321446836" alt="Food App logo"/>
+            <div className="title-name">Yummy<span  className="go-span">Go</span></div>
         </div>
         <div>
             <input placeholder="Search Food,Restaurant" className="searchBar"/>
@@ -26,14 +27,81 @@ function Header(){
 function Body(){
     return(
         <div className="items-conatiner">
-            <div className="item-card"></div>
-            <div className="item-card"></div>
-           <div className="item-card"></div>
-           <div className="item-card"></div>
-           <div className="item-card"></div>
-           <div className="item-card"></div>
-           <div className="item-card"></div>
-           <div className="item-card"></div>
+            
+            <div className="item-card">
+                <div >
+                    <img className="item-img" src="https://ik.imagekit.io/marSaQ/Image-symbol-icon-vector-by-Hoeda80-580x386.jpg"/>
+                </div>
+                <div className="item-res">Restaurant Name</div>
+                <div className="item-loc">Location</div>
+                <div className="item-rp">Rating | Price of two</div>
+            </div>
+           
+           <div className="item-card">
+                <div >
+                    <img className="item-img" src="https://ik.imagekit.io/marSaQ/Image-symbol-icon-vector-by-Hoeda80-580x386.jpg"/>
+                </div>
+                <div className="item-res">Restaurant Name</div>
+                <div className="item-loc">Location</div>
+                <div className="item-rp">Rating | Price of two</div>
+            </div>
+
+            <div className="item-card">
+                <div >
+                    <img className="item-img" src="https://ik.imagekit.io/marSaQ/Image-symbol-icon-vector-by-Hoeda80-580x386.jpg"/>
+                </div>
+                <div className="item-res">Restaurant Name</div>
+                <div className="item-loc">Location</div>
+                <div className="item-rp">Rating | Price of two</div>
+            </div>
+
+            <div className="item-card">
+                <div >
+                    <img className="item-img" src="https://ik.imagekit.io/marSaQ/Image-symbol-icon-vector-by-Hoeda80-580x386.jpg"/>
+                </div>
+                <div className="item-res">Restaurant Name</div>
+                <div className="item-loc">Location</div>
+                <div className="item-rp">Rating | Price of two</div>
+            </div>
+
+            <div className="item-card">
+                <div >
+                    <img className="item-img" src="https://ik.imagekit.io/marSaQ/Image-symbol-icon-vector-by-Hoeda80-580x386.jpg"/>
+                </div>
+                <div className="item-res">Restaurant Name</div>
+                <div className="item-loc">Location</div>
+                <div className="item-rp">Rating | Price of two</div>
+            </div>
+
+            <div className="item-card">
+                <div >
+                    <img className="item-img" src="https://ik.imagekit.io/marSaQ/Image-symbol-icon-vector-by-Hoeda80-580x386.jpg"/>
+                </div>
+                <div className="item-res">Restaurant Name</div>
+                <div className="item-loc">Location</div>
+                <div className="item-rp">Rating | Price of two</div>
+            </div>
+
+            <div className="item-card">
+                <div >
+                    <img className="item-img" src="https://ik.imagekit.io/marSaQ/Image-symbol-icon-vector-by-Hoeda80-580x386.jpg"/>
+                </div>
+                <div className="item-res">Restaurant Name</div>
+                <div className="item-loc">Location</div>
+                <div className="item-rp">Rating | Price of two</div>
+            </div>
+
+            <div className="item-card">
+                <div >
+                    <img className="item-img" src="https://ik.imagekit.io/marSaQ/Image-symbol-icon-vector-by-Hoeda80-580x386.jpg"/>
+                </div>
+                <div className="item-res">Restaurant Name</div>
+                <div className="item-loc">Location</div>
+                <div className="item-rp">Rating | Price of two</div>
+            </div>
+
+
+
         </div>
     )
 
